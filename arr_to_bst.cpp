9cd29a90ce54ee7c2f05d42d909f9937d5e24bb1
prernamadan <89862919+prernamadan@users.noreply.github.com>
2026-08-31 12:48:36 +0530

    Fix logic in array to BST conversion

--- arr_to_bst.cpp
+++ arr_to_bst.cpp
@@ -5,10 +5,11 @@ public:
         if(end < st)
         return NULL;
 
-        int mid= (st + end)/2;
-        TreeNode* root= new Node (nums[mid]);
-        root-> left= helper(nums, mid+1 , mid-1);
-        root-> right=helper(nums, st , end);
+        int mid= st + (end- st)/2;
+        TreeNode* root= new TreeNode (nums[mid]);
+        root-> left= helper(nums, st , mid-1);
+        root-> right=helper(nums, mid+1 , end);
+        return root;
 
     }
     TreeNode* sortedArrayToBST(vector<int>& nums) {
